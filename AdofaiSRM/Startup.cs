6ee170df6eec0158c7a4a1c116690eecd3a8b237
thijnmens <thijnsmulders04@gmail.com>
2022-06-11 23:06:59 +0200

    Weird dll loading issues with net.http

---
+++ AdofaiSRM/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using UnityModManagerNet;
+
+namespace AdofaiSRM
+{
+    internal static class Startup
+    {
+
+        internal static void Load(UnityModManager.ModEntry modEntry)
+        {
+            LoadAssembly("Mods/AdofaiSRM/System.Net.Http.dll");
+
+            Main.Load(modEntry);
+        }
+
+        private static void LoadAssembly(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                byte[] data = new byte[stream.Length];
+                stream.Read(data, 0, data.Length);
+                AppDomain.CurrentDomain.Load(data);
+            }
+        }
+    }
+}
